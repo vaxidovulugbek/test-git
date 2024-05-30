0aced00530a08f1a5d346aee99f60ca8b18a8d70
ulugbek <vulugb@gmail.com>
2024-05-30 11:57:06 +0500

    ss

--- src/components/Ont/Ont.jsx
+++ src/components/Ont/Ont.jsx
@@ -5,18 +5,42 @@ import { SlSocialVkontakte } from "react-icons/sl";
 import { FaOdnoklassniki } from "react-icons/fa6";
 import "./Contacts.scss";
 
+import GoogleMapReact from 'google-map-react';
+
+const AnyReactComponent = ({ text }) => <div>{text}</div>;
+
 function Ont() {
+    const defaultProps = {
+        center: {
+          lat: 10.99835602,
+          lng: 77.01502627
+        },
+        zoom: 11
+      };
   return (
     <section className="contacts py-[53px]">
       <div className="container-box">
         <div className="flex items-center gap-9">
           <div className="w-1/2 h-[422px] overflow-hidden">
-            <YMaps className="w-full h-full h-[422px]">
+            {/* <YMaps className="w-full h-full h-[422px]">
                 <Map className="w-full h-full h-[422px]" defaultState={{ center: [55.751574, 37.573856], zoom: 9 }}>
                     <Placemark className="w-full h-full h-[422px]" geometry={[55.751574, 37.573856]} />
                     <ZoomControl options={{ float: 'right' }} />
                 </Map>
-            </YMaps>
+            </YMaps> */}
+            <GoogleMapReact
+            className="w-full h-full h-[422px]"
+        bootstrapURLKeys={{ key: "" }}
+        defaultCenter={defaultProps.center}
+        defaultZoom={defaultProps.zoom}
+      >
+        <AnyReactComponent
+        className="w-full h-full h-[422px]"
+          lat={59.955413}
+          lng={30.337844}
+          text="My Marker"
+        />
+      </GoogleMapReact>
           </div>
           <div className="w-1/2 flex flex-col">
             <p className="text-[27px] font-semibold text-gray-700 tracking-wide mb-5">Наши контакты</p>
